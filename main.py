1c6ac004c8fb5ce6984b09f88160fb64e2a001ce
Samuel Boynton <samlingx@gmail.com>
2023-01-09 18:53:58 -0800

    Break out routes into their own file

--- main.py
+++ main.py
@@ -2,14 +2,20 @@ import logging
 import uvicorn
 
 from fastapi import Depends, FastAPI, HTTPException, Request, Response
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 
-from src.petofthemonth import crud, models, schemas
+from src.petofthemonth import models, routes
 from src.petofthemonth.database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+app.include_router(routes.router)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 @app.middleware("http")
 async def db_session_middleware(request: Request, call_next):
@@ -20,101 +26,6 @@ async def db_session_middleware(request: Request, call_next):
     finally:
         request.state.db.close()
     return response
-
-def get_db(request: Request):
-    return request.state.db
-
-@app.get("/")
-def read_root():
-    return "Hello world"
-
-# Users
-@app.get("/users/", response_model=list[schemas.User])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = crud.get_users(db, skip=skip, limit=limit)
-    return users
-
-@app.get("/users/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="user not found")
-    return db_user
-
-@app.post("/users/", response_model=schemas.User)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    return crud.create_user(db=db, user=user)
-
-@app.post("/users/{user_id}/pets/", response_model=schemas.Pet)
-def create_pet_for_owner(user_id: int, pet: schemas.PetCreate, db: Session = Depends(get_db)):
-    return crud.create_owner_pet(db=db, pet=pet, user_id=user_id)
-
-
-# Groups
-@app.get("/groups/", response_model=list[schemas.Group])
-def read_groups(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    groups = crud.get_groups(db, skip=skip, limit=limit)
-    return groups
-
-@app.get("/groups/{group_id}", response_model=schemas.Group)
-def read_group(group_id: int, db: Session = Depends(get_db)):
-    db_group = crud.get_group(db, group_id=group_id)
-    if db_group is None:
-        raise HTTPException(status_code=404, detail="Group not found")
-    return db_group
-
-@app.post("/groups/", response_model=schemas.Group)
-def create_group(group: schemas.GroupCreate, db: Session = Depends(get_db)):
-    return crud.create_group(db=db, group=group)
-
-@app.patch("/groups/{group_id}/users/{user_id}", response_model=schemas.Group)
-def update_group_users(group_id: int, user_id: int, db: Session = Depends(get_db)):
-    return crud.update_group_users(db, group_id=group_id, user_id=user_id)
-
-@app.patch("/groups/{group_id}/pets/{pet_id}", response_model=schemas.Group)
-def update_group_pets(group_id: int, pet_id: int, db: Session = Depends(get_db)):
-    return crud.update_group_pets(db, group_id=group_id, pet_id=pet_id)
-
-
-# Pets
-@app.get("/pets/", response_model=list[schemas.Pet])
-def read_pets(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    pets = crud.get_pets(db, skip=skip, limit=limit)
-    return pets
-
-@app.get("/pets/{pet_id}", response_model=schemas.Pet)
-def read_pet(pet_id: int, db: Session = Depends(get_db)):
-    db_pet = crud.get_pet(db, pet_id=pet_id)
-    if db_pet is None:
-        raise HTTPException(status_code=404, detail="Pet not found")
-    return db_pet
-
-@app.post("/pets/", response_model=schemas.Pet)
-def create_pet(pet: schemas.PetCreate, db: Session = Depends(get_db)):
-    return crud.create_pet(db=db, pet=pet)
-
-@app.patch("/pets/{pet_id}/owners/{owner_id}", response_model=schemas.Pet)
-def update_pet_owners(pet_id: int, user_id: int, db: Session = Depends(get_db)):
-    return crud.update_pet_owners(db, pet_id=pet_id, user_id=user_id)
-
-
-# Points
-@app.get("/points/", response_model=list[schemas.Point])
-def read_points(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    points = crud.get_points(db, skip=skip, limit=limit)
-    return points
-
-@app.get("/points/{point_id}", response_model=schemas.Point)
-def read_point(point_id: int, db: Session = Depends(get_db)):
-    db_point = crud.get_point(db, point_id=point_id)
-    if db_point is None:
-        raise HTTPException(status_code=404, detail="Point not found")
-    return db_point
-
-@app.post("/pets/{pet_id}/points/", response_model=schemas.Point)
-def create_point_for_pet(pet_id: int, point: schemas.PointCreate, db: Session = Depends(get_db)):
-    return crud.create_pet_point(db=db, point=point, pet_id=pet_id)
-
 
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ src/petofthemonth/routes.py
@@ -0,0 +1,100 @@
+from fastapi import APIRouter, Depends, Request, Response
+from . import crud, schemas
+from sqlalchemy.orm import Session
+
+router = APIRouter()
+
+def get_db(request: Request):
+    return request.state.db
+
+@router.get("/")
+def read_root():
+    return "Hello world"
+
+# Users
+@router.get("/users/", response_model=list[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
+
+@router.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="user not found")
+    return db_user
+
+@router.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    return crud.create_user(db=db, user=user)
+
+@router.post("/users/{user_id}/pets/", response_model=schemas.Pet)
+def create_pet_for_owner(user_id: int, pet: schemas.PetCreate, db: Session = Depends(get_db)):
+    return crud.create_owner_pet(db=db, pet=pet, user_id=user_id)
+
+
+# Groups
+@router.get("/groups/", response_model=list[schemas.Group])
+def read_groups(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    groups = crud.get_groups(db, skip=skip, limit=limit)
+    return groups
+
+@router.get("/groups/{group_id}", response_model=schemas.Group)
+def read_group(group_id: int, db: Session = Depends(get_db)):
+    db_group = crud.get_group(db, group_id=group_id)
+    if db_group is None:
+        raise HTTPException(status_code=404, detail="Group not found")
+    return db_group
+
+@router.post("/groups/", response_model=schemas.Group)
+def create_group(group: schemas.GroupCreate, db: Session = Depends(get_db)):
+    return crud.create_group(db=db, group=group)
+
+@router.patch("/groups/{group_id}/users/{user_id}", response_model=schemas.Group)
+def update_group_users(group_id: int, user_id: int, db: Session = Depends(get_db)):
+    return crud.update_group_users(db, group_id=group_id, user_id=user_id)
+
+@router.patch("/groups/{group_id}/pets/{pet_id}", response_model=schemas.Group)
+def update_group_pets(group_id: int, pet_id: int, db: Session = Depends(get_db)):
+    return crud.update_group_pets(db, group_id=group_id, pet_id=pet_id)
+
+
+# Pets
+@router.get("/pets/", response_model=list[schemas.Pet])
+def read_pets(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    pets = crud.get_pets(db, skip=skip, limit=limit)
+    return pets
+
+@router.get("/pets/{pet_id}", response_model=schemas.Pet)
+def read_pet(pet_id: int, db: Session = Depends(get_db)):
+    db_pet = crud.get_pet(db, pet_id=pet_id)
+    if db_pet is None:
+        raise HTTPException(status_code=404, detail="Pet not found")
+    return db_pet
+
+@router.post("/pets/", response_model=schemas.Pet)
+def create_pet(pet: schemas.PetCreate, db: Session = Depends(get_db)):
+    return crud.create_pet(db=db, pet=pet)
+
+@router.patch("/pets/{pet_id}/owners/{owner_id}", response_model=schemas.Pet)
+def update_pet_owners(pet_id: int, user_id: int, db: Session = Depends(get_db)):
+    return crud.update_pet_owners(db, pet_id=pet_id, user_id=user_id)
+
+
+# Points
+@router.get("/points/", response_model=list[schemas.Point])
+def read_points(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    points = crud.get_points(db, skip=skip, limit=limit)
+    return points
+
+@router.get("/points/{point_id}", response_model=schemas.Point)
+def read_point(point_id: int, db: Session = Depends(get_db)):
+    db_point = crud.get_point(db, point_id=point_id)
+    if db_point is None:
+        raise HTTPException(status_code=404, detail="Point not found")
+    return db_point
+
+@router.post("/pets/{pet_id}/points/", response_model=schemas.Point)
+def create_point_for_pet(pet_id: int, point: schemas.PointCreate, db: Session = Depends(get_db)):
+    return crud.create_pet_point(db=db, point=point, pet_id=pet_id)
+
